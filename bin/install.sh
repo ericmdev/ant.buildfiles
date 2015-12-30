6a50e31eed90044496facfd57ee1c32de6addd74
Eric Mugerwa <ericmdev@gmail.com>
2015-12-30 23:20:47 +0000

    Check if files exist

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,19 @@
-# Set bin directory.
-BINDIR=$(dirname $0)/bin
-echo $BINDIR
+# Define bin directory.
+bin=$(dirname $0)
+echo $bin
+
+# Define files.
+pdepend=$bin/phpDocumentor.phar
+phpdoc=$bin/phpDocumentor.phar
 
 # Acquire pdepend.
-wget http://static.pdepend.org/php/latest/pdepend.phar -P $BINDIR
-chmod +x $BINDIR/pdepend.phar
+if [ ! -f $pdepend ]; then
+  wget http://static.pdepend.org/php/latest/pdepend.phar -P $bin
+  chmod +x $pdepend
+fi
 
 # Acquire phpDocumentor.
-wget http://www.phpdoc.org/phpDocumentor.phar -P $BINDIR
-chmod +x $BINDIR/phpDocumentor.phar
+if [ ! -f $phpdoc ]; then
+  wget http://www.phpdoc.org/phpDocumentor.phar -P $bin
+  chmod +x $phpdoc
+fi
