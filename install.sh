baa9648246a428f38cd79039c11d066d3d498a26
Eric Mugerwa <ericmdev@gmail.com>
2015-12-30 20:36:38 +0000

    Acquire phpDocumentor

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ echo $BINDIR
 # Acquire pdepend.
 wget http://static.pdepend.org/php/latest/pdepend.phar -P $BINDIR
 chmod +x $BINDIR/pdepend.phar
+
+# Acquire phpDocumentor.
+wget http://www.phpdoc.org/phpDocumentor.phar -P $BINDIR
+chmod +x $BINDIR/phpDocumentor.phar
